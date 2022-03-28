4265064d3690f4f0b83dab983f9e667dc16b0221
Kavitha Shanmugan <kavitha@rapidinnovation.dev>
2022-03-28 11:53:12 +0530

    Create RobodogeCoin_2.sol

---
+++ RobodogeCoin_2.sol
@@ -0,0 +1,7 @@
+/******three layer sales halting mechanism.
+If we base it off of the "All time high” then the system would automatically know where those percentage drops would be and when those prices hit it would trigger the halt. So we use the all time high as a starting point to decide where those levels will be.
+Level 2 would be calculated from where level one is,
+and level three would be calculated from where level two is.
+If the third level gets hit after the halt Is over the system resets using that third level as the point as to where to calculate how far of a drop until the new level one halt is triggered****/
+
+
